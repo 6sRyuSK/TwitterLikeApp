92999602ddb5f3e926a6170bed7b1c48e5e20526
6sRyuSK <zikkyou19@gmail.com>
2020-05-30 18:19:16 +0900

    add _userid.vue

---
+++ pages/user/_userid.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ $route.params.userid }}</h1>
+    <h2>{{ user.username }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      user: {}
+    }
+  },
+  mounted () {
+    console.log(this.$route.params)
+  },
+  created () {
+    console.log('created!!!')
+    this.user = this.getUserData(this.$route.params.userid)
+    console.log(this.user)
+  },
+  methods: {
+    getUserData (userid) {
+      return {
+        userid,
+        username: userid + 'さん'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
